748c0aa6eb7589f29101a737903e379cf65fe53a
thilukshi-thamariselvam <thilukshi.thamariselvam@dmsswt.com>
2026-03-30 09:38:59 +0530

    initialize Spring Boot project

---
+++ src/main/java/com/app2/tasklytodo/TasklytodoApplication.java
@@ -0,0 +1,13 @@
+package com.app2.tasklytodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklytodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklytodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app2/tasklytodo/TasklytodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app2.tasklytodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklytodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
